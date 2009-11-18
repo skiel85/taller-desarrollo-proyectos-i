93ece791aac7c14ca94fc0970025b647acb43bda
mariano.converti <mariano.converti@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2009-11-18 12:43:49 +0000

    # agregué el esqueleto de la aplicación web.

---
+++ demo/ZoosManagementSystem/Controllers/AdministrationController.cs
@@ -0,0 +1,27 @@
+namespace ZoosManagementSystem.Controllers
+{
+    using System.Web.Mvc;
+
+    public class AdministrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Surveys()
+        {
+            return View();
+        }
+
+        public ActionResult Environments()
+        {
+            return View();
+        }
+
+        public ActionResult Animals()
+        {
+            return View();
+        }
+    }
+}
---
+++ demo/ZoosManagementSystem/Views/Home/Index.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="indexTitle" ContentPlaceHolderID="TitleContent" runat="server">
+    Administraci&oacute;n Inteligente de Zool&oacute;gicos - Home
+</asp:Content>
+
+<asp:Content ID="indexContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <h2><%= Html.Encode(ViewData["Message"]) %></h2>
+    
+    <p><strong>TODO</strong></p>
+</asp:Content>
