79fcad4fd7297e86b837c41cabeacd953faf451e
mariano.converti <mariano.converti@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2010-02-01 00:15:19 +0000

    # agregue los scripts para crear y rellenar la base de datos. Todavía tenemos que actualizar el script PopulateZoosManagementSystemDatabase.sql para agregar más datos a la base de datos.

---
+++ trunk/demo/Database/DropAndCreateZoosManagementSystemDatabase.cmd
@@ -0,0 +1,40 @@
+@echo off
+setlocal 
+%~d0
+cd "%~dp0"
+
+Set SERVERNAME="%1%"
+if "%1%"=="" set SERVERNAME=.\SQLEXPRESS
+
+echo.
+echo =====================================
+echo Creating ZoosManagementSystem database
+echo =====================================
+echo.
+
+echo Droping and creating the ZooManagementSystem database...
+
+@call SqlCmd -S %SERVERNAME% -E -i ".\DropAndCreateZoosManagementSystemDatabase.sql"
+IF ERRORLEVEL 1 GOTO ERROR
+
+echo Creating Stored Procedures into AdventureWorks2008 database...
+
+@call SqlCmd -S %SERVERNAME% -E -i ".\PopulateZoosManagementSystemDatabase.sql"
+IF ERRORLEVEL 1 GOTO ERROR
+
+echo.
+echo ================================================================
+echo ZoosManagementSystem database created and populated successfully!
+echo ================================================================
+GOTO FINISH
+
+:ERROR
+echo.
+echo ======================================
+echo An error occured. 
+echo Please review messages above.
+echo ======================================
+
+:FINISH
+REM @PAUSE
+exit %ERRORLEVEL%
---
+++ trunk/demo/Database/README.txt
@@ -0,0 +1,12 @@
+Instrucciones
+=============
+Para crear la Base de Datos hay que ejecutar el comando DropAndCreateZoosManagementSystemDatabase.cmd.
+
+
+Requerimientos
+==============
+* Microsoft® SQL Server® 2008 Express with Advanced Services (http://www.microsoft.com/downloads/details.aspx?familyid=B5D1B8C3-FDA5-4508-B0D0-1311D670E336)
+* Instance Name: SQLEXPRESS
+
+
+
