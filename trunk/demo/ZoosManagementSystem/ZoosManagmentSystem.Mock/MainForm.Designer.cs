db1640f0b97af4765840d292a007a3c1c5966a72
berta1108 <berta1108@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2010-02-10 03:12:40 +0000

    Agregados algunos controles a la app winforms del mock. Va a haber 3 consolas con mensajes, 1 para la aplicacion, y una para cada webservice mostrando valores y acciones ejecutadas. Agregada la otra referencia del WS de conditions al core.

--- trunk/demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/MainForm.Designer.cs
+++ trunk/demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/MainForm.Designer.cs
@@ -29,26 +29,111 @@
         private void InitializeComponent()
         {
             this.startServiceButton = new System.Windows.Forms.Button();
+            this.statusMessagesTextbox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // startServiceButton
             // 
-            this.startServiceButton.Location = new System.Drawing.Point(91, 86);
+            this.startServiceButton.Location = new System.Drawing.Point(16, 29);
             this.startServiceButton.Name = "startServiceButton";
             this.startServiceButton.Size = new System.Drawing.Size(75, 23);
             this.startServiceButton.TabIndex = 0;
-            this.startServiceButton.Text = "Start service";
+            this.startServiceButton.Text = "Start services";
             this.startServiceButton.UseVisualStyleBackColor = true;
             this.startServiceButton.Click += new System.EventHandler(this.startServiceButton_Click);
+            // 
+            // statusMessagesTextbox
+            // 
+            this.statusMessagesTextbox.Location = new System.Drawing.Point(9, 18);
+            this.statusMessagesTextbox.Multiline = true;
+            this.statusMessagesTextbox.Name = "statusMessagesTextbox";
+            this.statusMessagesTextbox.Size = new System.Drawing.Size(220, 124);
+            this.statusMessagesTextbox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.statusMessagesTextbox);
+            this.groupBox1.Location = new System.Drawing.Point(15, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 142);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "General messages";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.startServiceButton);
+            this.groupBox2.Location = new System.Drawing.Point(17, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(591, 72);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controls";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Location = new System.Drawing.Point(273, 116);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(226, 156);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Actions";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 18);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 124);
+            this.textBox2.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Location = new System.Drawing.Point(514, 116);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(226, 156);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Conditions";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 18);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 124);
+            this.textBox1.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.startServiceButton);
+            this.ClientSize = new System.Drawing.Size(767, 284);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Actions/Conditions Emulator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +141,13 @@
         #endregion
 
         private System.Windows.Forms.Button startServiceButton;
+        private System.Windows.Forms.TextBox statusMessagesTextbox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
