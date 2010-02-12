931d1d1d7e9b2bf9a3eb696105e4517ef1a52567
berta1108 <berta1108@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2010-02-12 14:12:11 +0000

    Agregado AnimalHealthService y las querys correspondientes. Agregada clase utilitaria para comparacion de fechas.

--- trunk/demo/ZoosManagementSystem/ZoosManagementSystem.Core/Switch/Service/AnimalHealthService.cs
+++ trunk/demo/ZoosManagementSystem/ZoosManagementSystem.Core/Switch/Service/AnimalHealthService.cs
@@ -2,10 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Mail;
+using System.Threading;
+
+using ZoosManagementSystem.Core.Foundation.Service;
+using ZoosManagementSystem.Core.Storage;
+using ZoosManagementSystem.Core.Util;
 
 namespace ZoosManagementSystem.Core.Switch.Service
 {
-    class AnimalHealthService
+    public class AnimalHealthService : BaseService
     {
+        private DbHelper dbHelper;
+        private List<Animal> animals;
+
+        public AnimalHealthService()
+        {
+            this.animals = new List<Animal>();
+        }
+
+        public override void Initialize()
+        {
+            this.dbHelper = new DbHelper();
+        }
+
+        protected override void OnStart()
+        {
+            while (this.running)
+            {
+                try
+                {
+                    foreach (Animal animalToMeasure in this.animals)
+                    {
+                        if (DateTimeComparer.CompareDate(DateTime.Now, animalToMeasure.NextHealthMeasure) == 0)
+                        {
+                            Responsible responsible = this.dbHelper.GetResponsible(animalToMeasure.Responsible.Id);
+                            this.SendNotificationEmail(responsible, animalToMeasure, animalToMeasure.NextHealthMeasure);
+
+                        }
+                    }
+
+                    Thread.Sleep(5000);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        private void LoadDataFromStorage()
+        {
+            try
+            {
+                this.animals = this.dbHelper.GetAllAnimals();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void SendNotificationEmail(Responsible responsible, Animal animal, DateTime healthMeasureDate)
+        {
+            string notificationSubject = String.Format("Health Measure notification. Date: [{0}] - Animal '{1}'.", healthMeasureDate, animal.Id);
+            string notificationBody = string.Empty;
+
+            MailMessage message = new MailMessage("from@address", responsible.Email, notificationSubject, notificationBody);
+
+            SmtpClient mailClient = new SmtpClient("Email Server Name");
+            mailClient.Send(message);
+        }
     }
 }
