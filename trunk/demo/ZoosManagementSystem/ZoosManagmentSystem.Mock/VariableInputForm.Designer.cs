b0fe7ec152cf6d8e776ac33f07d305fb65c819b5
berta1108 <berta1108@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2010-02-15 01:55:53 +0000

    Faltantes.

---
+++ trunk/demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/VariableInputForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ZoosManagmentSystem.Mock
+{
+    partial class VariableInputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.variableInputTextBox = new System.Windows.Forms.TextBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // variableInputTextBox
+            // 
+            this.variableInputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.variableInputTextBox.Location = new System.Drawing.Point(88, 35);
+            this.variableInputTextBox.Name = "variableInputTextBox";
+            this.variableInputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.variableInputTextBox.TabIndex = 0;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(49, 89);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(75, 23);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(146, 89);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // VariableInputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelButton;
+            this.ClientSize = new System.Drawing.Size(287, 134);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.variableInputTextBox);
+            this.Name = "VariableInputForm";
+            this.Text = "VariableInputForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox variableInputTextBox;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
---
+++ trunk/demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/VariableInputForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZoosManagmentSystem.Mock.EnvironmentEmulation;
+
+namespace ZoosManagmentSystem.Mock
+{
+    public partial class VariableInputForm : Form
+    {
+        private Guid environmentId;
+        private EnvironmentVariable variable;
+
+        public VariableInputForm()
+        {
+            InitializeComponent();
+        }
+
+        public VariableInputForm(Guid environmentId, EnvironmentVariable variable)
+        {
+            this.environmentId = environmentId;
+            this.variable = variable;
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.variable)
+            {
+                case EnvironmentVariable.Humidity:
+                    EnvironmentSimulator.SetHumidity(this.environmentId, float.Parse(this.variableInputTextBox.Text));
+                    break;
+                case EnvironmentVariable.Luminosity:
+                    EnvironmentSimulator.SetLuminosity(this.environmentId, float.Parse(this.variableInputTextBox.Text));
+                    break;
+                case EnvironmentVariable.Temperature:
+                    EnvironmentSimulator.SetTemperature(this.environmentId, float.Parse(this.variableInputTextBox.Text));
+                    break;
+                default:
+                    break;
+            }
+
+            this.Close();
+        }
+
+
+    }
+}
+
