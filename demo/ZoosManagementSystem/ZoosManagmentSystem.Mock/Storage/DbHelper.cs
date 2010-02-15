f902863cc5a5b43d3f778c1c486209f8fb325032
berta1108 <berta1108@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2010-02-15 13:41:05 +0000

    Agregada imple para Feeding en el mock.

--- demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/Storage/DbHelper.cs
+++ demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/Storage/DbHelper.cs
@@ -52,6 +52,54 @@ namespace ZoosManagmentSystem.Mock.Storage
 
         #endregion
 
+        #region Animal
+
+        public static List<Animal> GetAnimalsFromEnvironment(Guid environmentId)
+        {
+            var query = from e in DbHelper.entities.Animal
+                        where e.Environment.Id == environmentId
+                        select e;
+
+            return query.ToList();
+        }
+
+        public static Animal GetAnimal(Guid animalId)
+        {
+            var query = from e in DbHelper.entities.Animal
+                        where e.Id == animalId
+                        select e;
+
+            return query.First();
+        }
+
+        #endregion
+
+        #region FeedingTime
+
+        public List<FeedingTime> GetFeedingTimesFromAnimal(Guid animalId)
+        {
+            var query = from e in DbHelper.entities.FeedingTime.Include("Animal.Environment")
+                        where e.Animal.Id == animalId
+                        select e;
+
+            return query.ToList();
+        }
+
+        #endregion
+
+        #region Feeding
+
+        public static Feeding GetFeeding(Guid feedingId)
+        {
+            var query = from e in DbHelper.entities.Feeding
+                        where e.Id == feedingId
+                        select e;
+
+            return query.First();
+        }
+
+        #endregion
+
         #endregion
     }
 }
