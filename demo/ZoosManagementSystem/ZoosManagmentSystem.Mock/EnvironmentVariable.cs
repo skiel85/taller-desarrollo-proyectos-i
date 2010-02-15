ac52c47dc7e50747e2c25a185e109baef5f1e4b5
berta1108 <berta1108@4dc43d86-a608-11de-9ca8-173d21a9ba08>
2010-02-15 01:55:53 +0000

    Faltantes.

---
+++ demo/ZoosManagementSystem/ZoosManagmentSystem.Mock/EnvironmentVariable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZoosManagmentSystem.Mock
+{
+    public enum EnvironmentVariable
+    {
+        Humidity = 0,
+        Temperature = 1,
+        Luminosity = 2
+    }
+}
